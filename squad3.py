6d3fb4fb73f2c880b000e11c9c2e6b011f09563e
unknown <joshua.lowe@menloschool.org>
2021-05-17 00:59:24 -0700

    yeet

---
+++ squad3.py
@@ -0,0 +1,179 @@
+import json
+import numpy as np
+import random
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.optim.lr_scheduler as sched
+import torch.utils.data as data
+import util
+import transformers
+
+from args import get_train_args
+from collections import OrderedDict
+from datasets import load_metric
+from datasets import load_dataset
+from json import dumps
+from models import BiDAF
+from tensorboardX import SummaryWriter
+from transformers import AutoTokenizer
+from transformers import AutoModelForQuestionAnswering
+from transformers import TrainingArguments
+from transformers import Trainer
+from tqdm import tqdm
+from ujson import load as json_load
+from util import collate_fn, SQuAD
+
+def read_squad(path):
+    # open JSON file and load intro dictionary
+    with open(path, 'rb') as f:
+        squad_dict = json.load(f)
+
+    # initialize lists for contexts, questions, and answers
+    contexts = []
+    questions = []
+    answers = []
+    # iterate through all data in squad data
+    for group in squad_dict['data']:
+        for passage in group['paragraphs']:
+            context = passage['context']
+            for qa in passage['qas']:
+                question = qa['question']
+                # check if we need to be extracting from 'answers' or 'plausible_answers'
+                if 'plausible_answers' in qa.keys():
+                    access = 'plausible_answers'
+                else:
+                    access = 'answers'
+                for answer in qa[access]:
+                    # append data to lists
+                    contexts.append(context)
+                    questions.append(question)
+                    answers.append(answer)
+    # return formatted data lists
+    return contexts, questions, answers
+
+def add_end_idx(answers, contexts):
+    # loop through each answer-context pair
+    for answer, context in zip(answers, contexts):
+        # gold_text refers to the answer we are expecting to find in context
+        gold_text = answer['text']
+        # we already know the start index
+        start_idx = answer['answer_start']
+        # and ideally this would be the end index...
+        end_idx = start_idx + len(gold_text)
+
+        # ...however, sometimes squad answers are off by a character or two
+        if context[start_idx:end_idx] == gold_text:
+            # if the answer is not off :)
+            answer['answer_end'] = end_idx
+        else:
+            # this means the answer is off by 1-2 tokens
+            for n in [1, 2]:
+                if context[start_idx-n:end_idx-n] == gold_text:
+                    answer['answer_start'] = start_idx - n
+                    answer['answer_end'] = end_idx - n
+
+def add_token_positions(encodings, answers):
+    # initialize lists to contain the token indices of answer start/end
+    start_positions = []
+    end_positions = []
+    for i in range(len(answers)):
+        # append start/end token position using char_to_token method
+        start_positions.append(encodings.char_to_token(i, answers[i]['answer_start']))
+        end_positions.append(encodings.char_to_token(i, answers[i]['answer_end']))
+
+        # if start position is None, the answer passage has been truncated
+        if start_positions[-1] is None:
+            start_positions[-1] = tokenizer.model_max_length
+        # end position cannot be found, char_to_token found space, so shift position until found
+        shift = 1
+        while end_positions[-1] is None:
+            end_positions[-1] = encodings.char_to_token(i, answers[i]['answer_end'] - shift)
+            shift += 1
+    # update our encodings object with the new token-based start/end positions
+    encodings.update({'start_positions': start_positions, 'end_positions': end_positions})
+
+def preprocess(path):
+    # execute our read SQuAD function for training and validation sets
+    train_contexts, train_questions, train_answers = read_squad(path + 'train-v2.0.json')
+    test_contexts, test_questions, test_answers = read_squad(path + 'dev-v2.0.json')
+
+    # and apply the function to our two answer lists
+    add_end_idx(train_answers, train_contexts)
+    add_end_idx(val_answers, val_contexts)
+
+    tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased-distilled-squad")
+    train_encodings = tokenizer(train_contexts, train_questions, truncation=True, padding=True)
+    test_encodings = tokenizer(test_contexts, test_questions, truncation=True, padding=True)
+
+    # apply function to our data
+    add_token_positions(train_encodings, train_answers)
+    add_token_positions(test_encodings, test_answers)
+    train_dataset = Dataset.from_dict(train_encodings)
+    test_dataset = Dataset.from_dict(test_encodings)
+    return train_dataset, test_dataset
+
+def main(args):
+    path = '~/squad/data/'
+    train_dataset, test_dataset = preprocess(path)
+    args.save_dir = util.get_save_dir(args.save_dir, args.name, training=True)
+    log = util.get_logger(args.save_dir, args.name)
+    tbx = SummaryWriter(args.save_dir)
+    device, args.gpu_ids = util.get_available_devices()
+    log.info(f'Args: {dumps(vars(args), indent=4, sort_keys=True)}')
+    args.batch_size *= max(1, len(args.gpu_ids))
+
+    # Set random seed
+    log.info(f'Using random seed {args.seed}...')
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
+
+    # Get embeddings
+    log.info('Loading embeddings...')
+    word_vectors = util.torch_from_json(args.word_emb_file)
+    log.info('Building model...')
+    model = AutoModelForQuestionAnswering.from_pretrained("distilbert-base-uncased-distilled-squad")
+
+        ## setup GPU/CPU
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    # move model over to detected device
+    model.to(device)
+    # activate training mode of model
+    model.train()
+    # initialize adam optimizer with weight decay (reduces chance of overfitting)
+    optim = AdamW(model.parameters(), lr=5e-5)
+
+    # initialize data loader for training data
+    train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+
+    for epoch in range(30):
+        # set model to train mode
+        model.train()
+        # setup loop (we use tqdm for the progress bar)
+        loop = tqdm(train_loader, leave=True)
+        for batch in loop:
+            # initialize calculated gradients (from prev step)
+            optim.zero_grad()
+            # pull all the tensor batches required for training
+            input_ids = batch['input_ids'].to(device)
+            attention_mask = batch['attention_mask'].to(device)
+            start_positions = batch['start_positions'].to(device)
+            end_positions = batch['end_positions'].to(device)
+            # train model on batch and return outputs (incl. loss)
+            outputs = model(input_ids, attention_mask=attention_mask,
+                            start_positions=start_positions,
+                            end_positions=end_positions)
+            # extract loss
+            loss = outputs[0]
+            # calculate loss for every parameter that needs grad update
+            loss.backward()
+            # update parameters
+            optim.step()
+            # print relevant info to progress bar
+            loop.set_description(f'Epoch {epoch}')
+            loop.set_postfix(loss=loss.item())
+if __name__ == '__main__':
+    main(get_train_args())
